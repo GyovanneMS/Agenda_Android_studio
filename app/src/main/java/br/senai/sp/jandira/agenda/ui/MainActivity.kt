e828f1eafb85dd942c98ee784c8a0bc43dac10b8
GyovanneMS <Gyovannecontato@gmail.com>
2022-10-28 09:23:57 -0300

    Update

--- app/src/main/java/br/senai/sp/jandira/agenda/ui/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/agenda/ui/MainActivity.kt
@@ -1,13 +1,28 @@
 package br.senai.sp.jandira.agenda.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.senai.sp.jandira.agenda.R
+import br.senai.sp.jandira.agenda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ButtonFloatingAdd.setOnClickListener {
+
+            val abrirCadastro = Intent(this, NewContactActivity::class.java);
+
+            startActivity(abrirCadastro)
+
+        }
 
         //DAO - Objeto de acesso de dados
     }
--- app/src/main/java/br/senai/sp/jandira/agenda/ui/NewContactActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/agenda/ui/NewContactActivity.kt
@@ -3,10 +3,18 @@ package br.senai.sp.jandira.agenda.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.senai.sp.jandira.agenda.R
+import br.senai.sp.jandira.agenda.databinding.ActivityNewContactBinding
 
 class NewContactActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewContactBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityNewContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_new_contact)
         //ROOM começa a pensar na classe
     }
